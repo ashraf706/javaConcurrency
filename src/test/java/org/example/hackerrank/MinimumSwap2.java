d593c6475069b7f6f2ebe41dd211bec1474d165d
ashraful.hasan <ahasan@unrulygroup.com>
2020-04-11 17:43:40 +0100

    minimum swap 2 refactored

--- src/test/java/org/example/hackerrank/MinimumSwap2.java
+++ src/test/java/org/example/hackerrank/MinimumSwap2.java
@@ -4,11 +4,81 @@ import org.junit.Test;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Vector;
 
 import static junit.framework.TestCase.assertEquals;
 
 public class MinimumSwap2 {
+
+    @Test
+    public void minimumSwap() {
+        int result = minimumSwaps(new int[]{4, 3, 1, 2});
+        assertEquals(3, result);
+
+        result = minimumSwaps(new int[]{7, 1, 3, 2, 4, 5, 6});
+        assertEquals(5, result);
+        result = minimumSwaps(new int[]{2, 3, 4, 1, 5});
+        assertEquals(3, result);
+
+        result = minimumSwaps(new int[]{1, 3, 5, 2, 4, 6, 7});
+        assertEquals(3, result);
+
+        int[] bigArray = {2, 31, 1, 38, 29, 5, 44, 6, 12, 18, 39, 9, 48, 49, 13, 11, 7, 27, 14, 33, 50, 21, 46, 23, 15, 26, 8, 47, 40, 3, 32
+                , 22, 34, 42, 16, 41, 24, 10, 4, 28, 36, 30, 37, 35, 20, 17, 45, 43, 25, 19};
+        result = minimumSwaps(bigArray);
+        assertEquals(46, result);
+    }
+
+    static int minimumSwaps(int[] arr) {
+        Map<Integer, Integer> map = createUnorderedMapFrom(arr);
+        int[] unorderedItems = map.values().stream().mapToInt(i -> i).toArray();
+        int swapCount = 0;
+
+        for (int item : unorderedItems) {
+            if (!isItemInCorrectPosition(item, map)) {
+                swapItemInCorrectPosition(item, map);
+                swapCount++;
+            }
+        }
+
+        return swapCount;
+    }
+
+    public static void swapItemInCorrectPosition(int item, Map<Integer, Integer> map) {
+        int itemCorrectIndex = item - 1;
+        int temp = map.get(itemCorrectIndex);
+        Integer itemCurrentIndex = getIndex(map, item);
+
+        map.put(itemCorrectIndex, item);
+        map.put(itemCurrentIndex, temp);
+    }
+
+    public static boolean isItemInCorrectPosition(int item, Map<Integer, Integer> map) {
+        return map.get(item - 1) == item;
+    }
+
+    public static Map<Integer, Integer> createUnorderedMapFrom(int[] arr) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != i + 1) {
+                map.put(i, arr[i]);
+            }
+        }
+
+        return map;
+    }
+
+    public static Integer getIndex(Map<Integer, Integer> map, int value) {
+        return map.entrySet()
+                .stream()
+                .filter(entry -> entry.getValue() == value)
+                .map(Map.Entry::getKey)
+                .findFirst().get();
+    }
+
+    //=====================================================================================================================
+    //with array index
+    //=====================================================================================================================
 
     @Test
     public void minimumSwapWithArrayIndex() {
@@ -25,62 +95,11 @@ public class MinimumSwap2 {
         assertEquals(3, result);
     }
 
-    @Test
-    public void minimumSwap() {
-        int result = minimumSwaps(new int[]{4, 3, 1, 2});
-        assertEquals(3, result);
-        result = minimumSwaps(new int[]{7, 1, 3, 2, 4, 5, 6});
-        assertEquals(5, result);
-
-        result = minimumSwaps(new int[]{2, 3, 4, 1, 5});
-        assertEquals(3, result);
-
-        result = minimumSwaps(new int[]{1, 3, 5, 2, 4, 6, 7});
-        assertEquals(3, result);
-
-        int[] bigArray = {2, 31, 1, 38, 29, 5, 44, 6, 12, 18, 39, 9, 48, 49, 13, 11, 7, 27, 14, 33, 50, 21, 46, 23, 15, 26, 8, 47, 40, 3, 32
-                , 22, 34, 42, 16, 41, 24, 10, 4, 28, 36, 30, 37, 35, 20, 17, 45, 43, 25, 19};
-        result = minimumSwaps(bigArray);
-        assertEquals(46, result);
-
-    }
-
-    static int minimumSwaps(int[] arr) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-
-        Vector<Integer> vec = new Vector<>();
-
-        //add arr element to the map
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] != i + 1) {
-                vec.add(arr[i]);
-                map.put(i, arr[i]);
-            }
-
-        }
-        int swapCount = 0;
-        for (int position = 0; position < vec.size(); position++) {
-            int item = vec.get(position);
-            if (map.get(item - 1) != item) {
-                int temp = map.get(item - 1);
-                Integer currentPosition = getKey(map, item);
-                map.put(item - 1, item);
-                map.put(currentPosition, temp);
-                swapCount++;
-            }
-        }
-        return swapCount;
-    }
-
-    public static Integer getKey(Map<Integer, Integer> map, int value) {
-        return map.entrySet().stream().filter(entry -> entry.getValue() == value).map(Map.Entry::getKey).findFirst().get();
-    }
-
     static int minimumSwapsWithArrayIndex(int[] arr) {
         int count = 0;
         int index = 0;
         do {
-            index = swap(index, arr, arr[index]);
+            index = doSwap(index, arr, arr[index]);
             if (index == 0) {
                 count++;
             }
@@ -90,7 +109,7 @@ public class MinimumSwap2 {
         return count;
     }
 
-    static int swap(int index, int[] arr, int selectedItem) {
+    static int doSwap(int index, int[] arr, int selectedItem) {
         for (int i = index; i < arr.length; i++) {
             int currentItem = arr[i];
             if (currentItem > selectedItem) {
