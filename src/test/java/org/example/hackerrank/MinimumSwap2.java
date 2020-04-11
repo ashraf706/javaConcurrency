b66674de0c2d0550ce3211ac2dbe94e6ebf21be7
ashraful.hasan <ahasan@unrulygroup.com>
2020-04-11 12:04:56 +0100

    minimum swap 2 with map

--- src/test/java/org/example/hackerrank/MinimumSwap2.java
+++ src/test/java/org/example/hackerrank/MinimumSwap2.java
@@ -2,9 +2,27 @@ package org.example.hackerrank;
 
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static junit.framework.TestCase.assertEquals;
 
-public class MinimumSwap {
+public class MinimumSwap2 {
+
+    @Test
+    public void minimumSwapWithArrayIndex() {
+        int result = minimumSwapsWithArrayIndex(new int[]{4, 3, 1, 2});
+        assertEquals(3, result);
+
+        result = minimumSwapsWithArrayIndex(new int[]{7, 1, 3, 2, 4, 5, 6});
+        assertEquals(5, result);
+
+        result = minimumSwapsWithArrayIndex(new int[]{2, 3, 4, 1, 5});
+        assertEquals(3, result);
+
+        result = minimumSwapsWithArrayIndex(new int[]{1, 3, 5, 2, 4, 6, 7});
+        assertEquals(3, result);
+    }
 
     @Test
     public void minimumSwap() {
@@ -22,6 +40,31 @@ public class MinimumSwap {
     }
 
     static int minimumSwaps(int[] arr) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        //add arr element to the map
+        for (int i = 0; i < arr.length; i++) {
+            map.put(i, arr[i]);
+        }
+        int swapCount = 0;
+        for (int position = 0; position < arr.length; position++) {
+            int item = position + 1;
+            if (map.get(position) != item) {
+                int temp = map.get(position);
+                Integer currentPosition = getKey(map, item);
+                map.put(position, item);
+                map.put(currentPosition, temp);
+                swapCount++;
+            }
+        }
+        return swapCount;
+    }
+
+    public static Integer getKey(Map<Integer, Integer> map, int value) {
+        return map.entrySet().stream().filter(entry -> entry.getValue() == value).map(Map.Entry::getKey).findFirst().get();
+    }
+
+    static int minimumSwapsWithArrayIndex(int[] arr) {
         int count = 0;
         int index = 0;
         do {
