e5ac00c770fb2c76a78b8a4b886ed95f67fb8a9e
Ashraful Hasan <ashraf706@hotmail.com>
2020-05-28 09:07:08 +0100

    re-organise a bit

--- src/test/java/org/example/codility/OddOccurrencesInArray.java
+++ src/test/java/org/example/codility/OddOccurrencesInArray.java
@@ -9,11 +9,21 @@ import static org.junit.Assert.assertEquals;
 public class OddOccurrencesInArray {
     @Test
     public void oddOccurrence() {
-        assertEquals(7, solution2(new int[]{9, 20, 3, 5, 9, 3, 9, 20, 7, 9, 5}));
+        assertEquals(7, solution(new int[]{9, 20, 3, 5, 9, 3, 9, 20, 7, 9, 5}));
         //assertEquals(7, solution(new int[]{9, 3, 9, 3, 9, 7, 9}));
     }
 
     public int solution(int[] A) {
+        // write your code in Java SE 8
+        int elem = 0;
+
+        for (int i = 0; i < A.length; i++) {
+            elem ^= A[i];
+        }
+        return elem;
+    }
+
+    public int solution1(int[] A) {
         ArrayList<Integer> lst = new ArrayList<>();
         for (int i = 0; i < A.length; i++) {
             if (lst.contains(A[i])) {
@@ -23,15 +33,5 @@ public class OddOccurrencesInArray {
             }
         }
         return lst.size() > 0 ? lst.get(0) : 0;
-    }
-
-    public int solution2(int[] A) {
-        // write your code in Java SE 8
-        int elem = 0;
-
-        for (int i = 0; i < A.length; i++) {
-            elem ^= A[i];
-        }
-        return elem;
     }
 }
